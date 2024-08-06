d3807a46e66129d51376c15527ea969f54baaa85
zachgalbs <106509725+a-hiremath@users.noreply.github.com>
2024-08-05 21:55:10 -0700

    perchance did a thing or too

--- WaveWork/WaveWork/ContentView.swift
+++ WaveWork/WaveWork/ContentView.swift
@@ -2,9 +2,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Hello, world!")
+        NavigationView {
+            GeometryReader { geometry in
+                VStack {
+                    Text("wavework")
+                        .fontWeight(.bold)
+                        .dynamicTypeSize(.xxxLarge)
+                        .position(CGPoint(x: geometry.size.width * 0.5, y:50))
+                        
+                    
+                    NavigationLink(destination: NewScreenView()) {
+                        Text("blocked applications")
+                            .padding(18)
+                            .background(Color.blue)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(12)
+                    }
+                    .position(CGPoint(x: geometry.size.width * 0.5, y: geometry.size.height * -0.1))
+                    
+                    NavigationLink(destination:SocialView()){
+                        Text("social")
+                            .padding(18)
+                            .background(Color.blue)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(12)
+                    }
+                    .position(CGPoint(x:CGFloat((geometry.size.width * 0.5)), y: CGFloat(geometry.size.height * -0.35))
+                    
+                )}
+            }
         }
+    }
+}
+
+struct NewScreenView: View {
+    var body: some View {
+        VStack {
+            
+            Text("applicators 😾")
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            
+        }
+        .navigationBarTitle("blocked applications", displayMode: .inline)
+    }
+}
+
+struct SocialView: View {
+    var body: some View {
+        VStack{
+            
+            Text("buddies 🐀")
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            
+            
+        }
+        .navigationBarTitle("thingington",displayMode: .inline)
     }
 }
 
