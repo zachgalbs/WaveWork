c0fc4442d0f28ce5df09f8634f83acfd573ee1d5
zachgalbs <zacharythomasgalbraith@gmail.com>
2024-08-05 20:18:01 -0700

    init commit

---
+++ WaveWork/WaveWorkTests/WaveWorkTests.swift
@@ -0,0 +1,17 @@
+//
+//  WaveWorkTests.swift
+//  WaveWorkTests
+//
+//  Created by Zachary Galbraith on 8/5/24.
+//
+
+import Testing
+@testable import WaveWork
+
+struct WaveWorkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
