8671f263f084d0c6c8d3b2b9416acda84f428cba
zachgalbs <106509725+a-hiremath@users.noreply.github.com>
2024-08-05 20:30:00 -0700

    init commit

---
+++ WaveWork/WaveWork/WaveWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveWorkApp.swift
+//  WaveWork
+//
+//  Created by Aditya Hiremath on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct WaveWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
